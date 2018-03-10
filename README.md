7ac29b61570d8903d40b42e8f34aaed754d2e6ba
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-03-10 19:12:15 +0900

    Copy from matsubara0507/planet-haskell-jp-demo

--- README.md
+++ README.md
@@ -1,45 +1,9 @@
-Haskell Antenna
-===============
+# Haskell Antenna
 
-Haskellに関する日本語情報の更新をまとめて表示するWebサイト／APIです。
+Haskellに関する日本語情報の更新をまとめて表示するWebサイトです。
 
-使い方
-------
+Collect Haskeller's post in Japanese (like [Planet Haskell](https://planet.haskell.org/)).
 
-事前にlocalhost:6379でRedis Serverをたてておく必要があります。
-
-```shell
-$ stack build
-$ stack exec antenna-exe -- --env=local
-```
-
-Haddock
--------
-
-```shell
-$ stack haddock --open antenna
-```
-
-フィードの追加方法
-------------------
-
-1. config/feeds.yaml に情報を記載
-2. static/image/logo にアイコンを追加
-3. <https://github.com/haskell-jp/antenna/compare> よりPull Requestを送って下さい
-
-アイコンに関するガイドライン
-----------------------------
-
-各サービスのアイコンは以下のガイドラインに沿って使用しています。
-
-* Qiita - <http://help.qiita.com/ja/articles/others-brand-guideline>
-* Reddit - <https://www.reddit.com/wiki/licensing>
-* Stack Exchange - <https://stackexchange.com/legal/trademark-guidance>
-
-----
-
-Inspired by
-
-- [Awesome Haskell](https://haskell.libhunt.com/)
-- [Haskell - News](https://www.haskell.org/news)
-
+- Posts are collected by [ScrapBook](https://github.com/matsubara0507/scrapbook)
+- fork on [matsubara0507/planet-haskell-jp-demo](https://github.com/matsubara0507/planet-haskell-jp-demo)
+- Redesign from [lotz84's Antenna](https://github.com/haskell-jp/antenna/tree/17766399a31bbc7a46423802bead2eccc4dad6a0)
--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,129 @@
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Antenna (app)
-import Antenna.Config (getEnv)
+import           Prelude                       hiding (div, head, id, span)
+
+import           Control.Lens                  (view, (^.))
+import           Control.Monad                 ((<=<))
+import           Control.Monad.IO.Class        (liftIO)
+import           Data.Extensible
+import           Data.List                     (sortOn)
+import           Data.Maybe                    (listToMaybe)
+import           Data.String                   (IsString, fromString)
+import           Data.Text                     (Text, unpack)
+import qualified Data.Text.IO                  as T
+import qualified Data.Text.Lazy.IO             as TL
+import           Data.Time
+import qualified Data.Yaml                     as Y
+import           ScrapBook                     (collect, fetch, toSite, write)
+import qualified ScrapBook
+import           System.Directory              (createDirectoryIfMissing)
+import           System.Environment            (getArgs)
+import           System.FilePath               (dropFileName)
+import           Text.Blaze.Html.Renderer.Text (renderHtml)
+import           Text.Blaze.Html5              hiding (main)
+import           Text.Blaze.Html5.Attributes   (class_, href, id, lang, rel,
+                                                type_)
 
 main :: IO ()
-main = app =<< getEnv
+main = (listToMaybe <$> getArgs) >>= \case
+  Nothing   -> error "please input config file path."
+  Just path -> generate path =<< readConfig path
 
+readConfig :: FilePath -> IO ScrapBook.Config
+readConfig = either (error . show) pure <=< decodeFileEither'
+ where
+  decodeFileEither' path =
+    fmap (ScrapBook.updateFileName feed' path) <$> Y.decodeFileEither path
+
+generate :: FilePath -> ScrapBook.Config -> IO ()
+generate path config = either (error . show) pure <=< collect $ do
+  let sites = fmap toSite $ config ^. #sites
+  posts <- concat <$> mapM fetch sites
+
+  writeFeed (dropFileName path ++ name) =<< write config feed' posts
+
+  writeHtml "./index.html" $ tabNav baseUrl Posts $ mapM_
+    postToHtml
+    (take 50 . reverse $ sortOn (view #date) posts)
+
+  writeHtml "./sites.html" $ tabNav baseUrl Sites $ mapM_
+    siteToHtml
+    (sortOn (view #title) sites)
+ where
+  name    = ScrapBook.fileName config feed'
+  baseUrl = maybe "" (view #baseUrl) $ config ^. #feed
+
+data Tab
+  = Posts
+  | Sites
+  deriving (Show, Eq)
+
+tabNav :: Text -> Tab -> Html -> Html
+tabNav baseUrl selectedTab list = do
+  div ! class_ "tabnav" $ do
+    div ! class_ "float-right" $
+      a ! href (addBaseUrl "/feed") ! class_ "tabnav-extra" $ "feed"
+    nav ! class_ "tabnav-tabs" $ do
+      tab "/"      (selectedTab == Posts) "Post"
+      tab "/sites" (selectedTab == Sites) "Sites"
+  ul ! class_ "list-style-none" $ list
+ where
+  tab path selected = a ! href (addBaseUrl path) !
+    class_ ("tabnav-tab " `mappend` if selected then "selected" else "")
+  addBaseUrl = fromText . mappend baseUrl
+
+writeFeed :: FilePath -> Text -> ScrapBook.Collecter ()
+writeFeed path txt = liftIO $ writeFileWithDir path txt
+
+writeHtml :: FilePath -> Html -> ScrapBook.Collecter ()
+writeHtml path bodyHtml =
+  liftIO . TL.writeFile path . renderHtml $ docTypeHtml ! lang "jp" $ do
+    head $ do
+      title "Planet Haskell (JP)"
+      link ! rel "stylesheet" ! type_ "text/css" ! href
+        "https://cdnjs.cloudflare.com/ajax/libs/Primer/10.0.0-rc.21/build.css"
+    body $ div ! class_ "container-md" $ do
+      h1 ! id "header" $ "Planet Haskell (JP)"
+      bodyHtml
+
+postToHtml :: ScrapBook.Post -> Html
+postToHtml post = li ! class_ "border-bottom" $ do
+  h3 $ a' ! href (fromText $ post ^. #url) $ toHtml (post ^. #title)
+  p $ do
+    let site = post ^. #site
+    toHtml $ mconcat ["by ", site ^. #author]
+    " on "
+    span $ a' ! href (fromText $ site ^. #url) $ toHtml (site ^. #title)
+    toHtml $ mconcat [" at ", formatTimeToDate $ unpack (post ^. #date)]
+
+siteToHtml :: ScrapBook.Site -> Html
+siteToHtml site = li ! class_ "border-bottom" $ do
+  h3 $ a' ! href (fromText $ site ^. #url) $ toHtml (site ^. #title)
+  p $ toHtml $ mconcat ["by ", site ^. #author]
+
+a' :: Html -> Html
+a' = a ! class_ "link-gray-dark"
+
+feed' :: ScrapBook.Format
+feed' = embedAssoc $ #feed @= ()
+
+writeFileWithDir :: FilePath -> Text -> IO ()
+writeFileWithDir path txt = do
+  createDirectoryIfMissing True $ dropFileName path
+  T.writeFile path txt
+
+fromText :: IsString a => Text -> a
+fromText = fromString . unpack
+
+formatTimeToDate :: String -> String
+formatTimeToDate =
+  maybe "" (formatTime defaultTimeLocale "%B %d, %Y") . formatTimeFromRFC3339
+
+-- take 10 == take (length "2018-02-02")
+formatTimeFromRFC3339 :: String -> Maybe UTCTime
+formatTimeFromRFC3339 =
+  parseTimeM True defaultTimeLocale (iso8601DateFormat Nothing) . take 10
