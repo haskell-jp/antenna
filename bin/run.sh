7e01a1c506170cb8a3fbca522d2e30d7bd323f47
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2018-07-24 19:22:46 +0900

    Fix to remove temp dir before clone

--- bin/run.sh
+++ bin/run.sh
@@ -3,6 +3,7 @@
 set -eux
 
 cd /work
+rm -rfd temp
 git clone --depth 1 -b gh-pages "https://${GH_TOKEN}@github.com/haskell-jp/antenna.git" temp
 cp sites.yaml temp/sites.yaml
 cd temp
@@ -12,5 +13,3 @@ git status
 git add -A
 git diff --quiet && git diff --staged --quiet || git commit -am "[skip ci] Update planet haskell. See https://haskell.jp/antenna/ for new entries!"
 git push origin gh-pages
-cd ../
-rm -rfd temp
